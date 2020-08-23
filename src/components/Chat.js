6ee6549a49397af9d4d3bbc568febaee4ba791f9
Jessica Neary <jessicafneary@gmail.com>
2020-08-23 15:35:01 +1200

    Styled chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -23,13 +23,23 @@ const Chat = ({ room, user }) => {
     }
     return (
         <div className="col-8 bg-white d-flex flex-column justify-content-between">
-            <div className="py-4">
-                {room.chatLog.map(message => (
-                    <div key={message.id}>
-                        <strong>{message.user.name}: </strong>
-                        {message.text}
-                    </div>
-                ))}
+            <div className="message-log">
+                <div className="py-4 text-left">
+                    {room.chatLog.map(message => user.id === message.user.id ? 
+                        <div className="d-flex m-1" key={message.id}>
+                            <div className="bg-info text-white shadow-sm px-2 py-1 chat-bubble ml-auto">{message.text}</div>
+                            <div className="align-self-end ml-2">
+                                :<strong>{message.user.name}</strong>
+                            </div>
+                        </div> :
+                        <div className="d-flex m-1" key={message.id}>
+                            <div className="align-self-end mr-2">
+                                <strong>{message.user.name}</strong>:
+                            </div>
+                            <div className="bg-purple text-white shadow-sm px-2 py-1 chat-bubble">{message.text}</div>
+                        </div>
+                    )}
+                </div>
             </div>
             <div>
             <textarea className="form-control" value={input} onChange={handleInput} />
